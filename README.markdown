40120be33099d4a609263bbc6a2caee3ef8a1df7
Chris Batchelor <chris@firegoby.com>
2016-03-11 19:09:16 +0000

    v1.1.0RC1 - Add backup, restore, replay, enhanced tracking, more config options. Bug fixes.

--- README.markdown
+++ README.markdown
@@ -2,15 +2,62 @@
 
 ## Installation
 
-1. Download and upload the 'db_sync' folder to your Symphony 'extensions' folder.
-2. Enable the extension by selecting "Database Synchroniser" in the list and choose Enable from the with-selected menu, then click Apply.
+1. Add the `db_sync` folder to your Symphony `extensions` folder.
+2. Go to `Symphony Admin` > `System` > `Preferences`
+3. Enable the extension by selecting `Database Synchroniser` in the list and choose `Enable` from the with-selected menu, then click `Apply`.
 
 ## Warning
 
-As of version 0.7, queries are stored in a file named `db_sync.sql` in your `/manifest` folder. This file is visible to anyone,  therefore I strongly advise that this extension only be enabled on development environments. Don't deploy it to production, or disable it entirely by looking for `db_sync` in Symphony's config file.
+As of version 0.7, queries are stored (only by default, see `Version 1.1.0` notes below) in a file named `db_sync.sql` in your `/manifest` folder. This file is visible to anyone,  therefore I strongly advise that this extension only be enabled on development environments. Don't deploy it to production, or disable it entirely by looking for `db_sync` in Symphony's config file. See additional notes re: Version 1.1.0 for options to change the Query Log location.
 
 ## Disclaimer
 
 While this extension has worked well for my own projects, I can't guarantee its stability for your own. My workflow when using a development/staging/production environment is to install this extension on the development server only. When making a release I pull the production database back to staging where I apply the db_sync SQL file. If all goes well after testing, I back up production and run the same db_sync file there. The file is then removed locally and I can continue developing towards another release.
 
 Please, please, please back up your production database before applying any structural changes.
+
+## Version 1.1.0 (additions not by the original author)
+
+**Please Note**: All the new features of v1.1.0 are **Opt-In**, by default there's no change to how the extension functioned in previous versions. (aside from the bug fixes).
+
+ - **Fixes**: Compatibility with Order Entries extension (thanks to Jonathan Mifsud)
+
+ - **Fixes**: Compatibility with Symphony 2.4+ API for adding Author name to Query Log comments
+
+ - **New Feature**: Customise the Query Log location via `config.php`, now relative to project root (remains by default as `/manifest/db_sync.sql`).
+
+ - **New Feature**: Preferences panel button to replay all queries in the current Query Log. When enabled it *disables* Query Logging, thus it is designed for use on non-development deployments (i.e. when using dual-manifest setup) to allow easy DB synchronisation from a development's uploaded Query Log. After synchronisation the Query log is then archived as e.g. `/manifest/db_sync.sql.replayed.2016-03-05-12-35-01` to prevent duplicate replay (but keeping a record of past synchronisations). Disabled by default, enable it via `config.php` by setting `enable_replay` to `yes` for any relevant non-development deployments (e.g. testing, production, etc). See **An Example Setup & Workflow** section below for step-by-step guide.
+
+ - **New Feature**: Backup and Restore your Symphony Database with buttons on the Preferences page. Uses `mysqldump` and `gzip` behind the scenes to make a full, zipped copy of your current database. Restoring the database is likewise handled behind the scenes by `mysql` and `gunzip`. The exact location of all four binaries are easily edited in `config.php`.
+
+ - **New Feature**: Track content changes, either as Commented Queries (non-replayed, simply logged as comments for manual review) or as Full Queries (replayed like any other tracked query). Disabled by default, enable it via `config.php` by setting `track_content` to `comment` (for commented only) or `yes` (for full tracking).
+
+ - **New Feature**: Track Author changes, either as Commented Queries (non-replayed, simply logged as comments for manual review) or as Full Queries (replayed like any other tracked query). Disabled by default, enable it via `config.php` by setting `track_authors` to `comment` (for commented only) or `yes` (for full tracking).  
+**Please Note**: This feature even when enabled **ignores** updates to the Author's `last_seen` column since an `UPDATE` is made to that column on each page load in the Admin, thus including it in the Query Log generates an large number of useless entries.
+
+### An Example Setup & Workflow
+
+**Prerequisite**: Dual/Symlinked Manifest folder setup, expects a different `config.php` for development and production environments.
+
+1. Install the extension.
+
+2. Customise both `config.php` files to set `'db_sync'` with `'log_dir' => '/sql/'` (note both slashes).
+
+3. In your production `config.php` set `'db_sync'` with `'enable_replay' => 'yes'`
+
+4. Add `RewriteRule ^sql/(.*)$ - [F]` to `.htaccess` (or Apache config) to prevent access to `/sql` directory in production.
+
+5. Track `/sql` directory under version control.
+
+6. Develop as usual on the your development version.
+
+7. Upload (via version control, or however else, FTP etc) your changes (including `/sql/` dir) to your production deployment.
+
+8. In `Symphony > Preferences` backup your current production deployment with the `Backup Database` button.
+
+9. Replay your development Query Log in production with `Synchronise Database from Logs` button.
+
+10. Check everything worked as expected, if something went wrong, restore from backup with the `Restore Database from Last Backup` button. 
+
+If something did go wrong during synchronisation you'll need to manually investigate which queries caused the problems, look in the archived Query Log in e.g. `/sql/db_sync.sql.replayed.2016-03-05-12-35-01`
+
--- extension.driver.php
+++ extension.driver.php
@@ -3,43 +3,152 @@
 	Class extension_db_sync extends Extension {
 		
 		public static $meta_written = FALSE;
-		
+
+        private static $dont_log_this_run = FALSE;
+
 		public function getSubscribedDelegates() {
 			return array(
 				array(
 					'page'		=> '/backend/',
 					'delegate'	=> 'PostQueryExecution',
 					'callback'	=> 'log'
-				)
+				),
+                array(
+                    'page' => '/system/preferences/',
+                    'delegate' => 'AddCustomPreferenceFieldsets',
+                    'callback' => 'appendPreferences'
+                )
 			);
 		}
 		
 		public function install() {
 			Symphony::Configuration()->set('enabled', 'yes', 'db_sync');
+			Symphony::Configuration()->set('enable_replay', 'no', 'db_sync');
+            Symphony::Configuration()->set('track_authors', 'no', 'db_sync');
+            Symphony::Configuration()->set('track_content', 'no', 'db_sync');
+            Symphony::Configuration()->set('log_file', 'db_sync.sql', 'db_sync');
+            Symphony::Configuration()->set('log_dir', '/manifest/', 'db_sync');
+            Symphony::Configuration()->set('mysql_bin', '/usr/local/bin/mysql', 'db_sync');
+            Symphony::Configuration()->set('mysqldump_bin', '/usr/local/bin/mysqldump', 'db_sync');
+            Symphony::Configuration()->set('gzip_bin', '/usr/bin/gzip', 'db_sync');
+            Symphony::Configuration()->set('gunzip_bin', '/usr/bin/gunzip', 'db_sync');
 			Symphony::Configuration()->write();
 			return TRUE;
 		}
+
+		public function update($previousVersion = false) {
+			if(version_compare($previousVersion, '1.1', '<')) {
+                Symphony::Configuration()->set('enable_replay', 'no', 'db_sync');
+                Symphony::Configuration()->set('track_authors', 'no', 'db_sync');
+                Symphony::Configuration()->set('track_content', 'no', 'db_sync');
+                Symphony::Configuration()->set('log_file', 'db_sync.sql', 'db_sync');
+                Symphony::Configuration()->set('log_dir', '/manifest/', 'db_sync');
+                Symphony::Configuration()->set('mysql_bin', '/usr/local/bin/mysql', 'db_sync');
+                Symphony::Configuration()->set('mysqldump_bin', '/usr/local/bin/mysqldump', 'db_sync');
+                Symphony::Configuration()->set('gzip_bin', '/usr/bin/gzip', 'db_sync');
+                Symphony::Configuration()->set('gunzip_bin', '/usr/bin/gunzip', 'db_sync');
+                Symphony::Configuration()->write();
+                return TRUE;
+            }
+        }
 		
 		public function uninstall() {
-			if (file_exists(MANIFEST . '/db_sync.sql')) unlink(MANIFEST . '/db_sync.sql');
 			Symphony::Configuration()->remove('db_sync');
 			Symphony::Configuration()->write();
 		}
+
+        public function appendPreferences($context) {
+
+            // Create preference group
+            $group = new XMLElement('fieldset');
+            $group->setAttribute('class', 'settings');
+            $group->appendChild(new XMLElement('legend', __('Database Synchroniser')));
+
+            // create control frame
+            $div = new XMLElement('div', NULL, array('id' => 'db_sync_control', 'class' => 'label'));
+            $span = new XMLElement('span', NULL, array('class' => 'frame'));
+
+            //append action button
+			if(Symphony::Configuration()->get('enable_replay', 'db_sync') == 'yes') {
+                $span->appendChild(new XMLElement('button', __('Synchronise Database from Query Log'), array('name' => 'action[db-sync-replay]', 'type' => 'submit')));
+            }
+            $span->appendChild(new XMLElement('button', __('Backup Database'), array('name' => 'action[db-sync-backup]', 'type' => 'submit')));
+            $span->appendChild(new XMLElement('button', __('Restore Database from Last Backup'), array('name' => 'action[db-sync-restore]', 'type' => 'submit')));
+            $div->appendChild($span);
+            $group->appendChild($div);
+
+            $results = '';
+
+            $latest_backup = self::latestBackupFile();
+            if ($latest_backup) {
+                $results = "<strong>Lastest backup: </strong>" . $latest_backup;
+            }
+
+            if(isset($_POST['action']['db-sync-replay'])){
+                $results = $this->replay();
+            }
+            else if(isset($_POST['action']['db-sync-backup'])){
+                $results = $this->backup();
+            }
+            else if(isset($_POST['action']['db-sync-restore'])){
+                $results = $this->restore();
+            }
+            
+            if ($results) { // Append output results
+                $group->appendChild(new XMLElement('p', __($results)), array());
+            }
+
+            // Append new preference group
+            $context['wrapper']->appendChild($group);
+        }
 		
 		public static function log($context) {
 			if(Symphony::Configuration()->get('enabled', 'db_sync') == 'no') return;
+			if(Symphony::Configuration()->get('enable_replay', 'db_sync') == 'yes') return;
+            if(self::$dont_log_this_run) return;
 			
 			$query = $context['query'];
+
+            // ensure one line per statement, no line breaks or extraneous whitespace
+            $query = trim(preg_replace('/\s+/', ' ', $query)); 
+
+			// append query delimeter if it doesn't exist
+			if (!preg_match('/;$/', $query)) $query .= ";";
+
 
 			$tbl_prefix = Symphony::Configuration()->get('tbl_prefix', 'database');
 
 			/* FILTERS */
 			// only structural changes, no SELECT statements
 			if (!preg_match('/^(insert|update|delete|create|drop|alter|rename)/i', $query)) return;
-			// un-tracked tables (sessions, cache, authors)
-			if (preg_match("/{$tbl_prefix}(authors|cache|forgotpass|sessions)/i", $query)) return;
+
+			// un-tracked tables (sessions, cache)
+			if (preg_match("/{$tbl_prefix}(cache|forgotpass|sessions)/i", $query)) return;
+
+            // track, or not, authors
+            // include as comments instead of ignoring completely if track_authors enabled
+            if (preg_match("/{$tbl_prefix}(authors)/i", $query)) {
+                // always ignore 'last seen' tracking since it's queried on every admin page load
+                if (preg_match("/^UPDATE {$tbl_prefix}authors SET \`last_seen\`/", trim($query))) return; 
+
+                if (Symphony::Configuration()->get('track_authors', 'db_sync') == 'no') {
+                    return;
+                }
+                else if (Symphony::Configuration()->get('track_authors', 'db_sync') == 'comment') {
+                    $query = '-- ' . $query; // commentify
+                }
+            }
+
 			// content updates in tbl_entries (includes tbl_entries_fields_*)
-			if (preg_match('/^(insert|delete|update)/i', $query) && preg_match("/({$config->tbl_prefix}entries)/i", $query)) return;
+            // include as comments instead of ignoring completely if track_content enabled
+            if (preg_match('/^(insert|delete|update)/i', $query) && preg_match("/({$tbl_prefix}entries)/i", $query)) {
+                if (Symphony::Configuration()->get('track_content', 'db_sync') == 'no') {
+                    return;
+                }
+                else if (Symphony::Configuration()->get('track_content', 'db_sync') == 'comment') {
+                    $query = '-- ' . $query; // commentify
+                }
+            }
 
 			$line = '';
 
@@ -47,7 +156,7 @@
 
 				$line .= "\n" . '-- ' . date('Y-m-d H:i:s', time());
 
-				$author = Administration::instance()->Author;
+				$author = Symphony::Engine()->Author();
 				if (isset($author)) $line .= ', ' . $author->getFullName();
 
 				$url = Administration::instance()->getCurrentPageURL();
@@ -59,18 +168,115 @@
 
 			}
 
-			$query = trim($query);
-
-			// append query delimeter if it doesn't exist
-			if (!preg_match('/;$/', $query)) $query .= ";";
-
+            // separate each query log with a blank line
 			$line .= $query . "\n";
 			
-			$logfile = MANIFEST . '/db_sync.sql';
-			$handle = @fopen($logfile, 'a');
-			fwrite($handle, $line);
-			fclose($handle);
-			
+            $handle = @fopen(self::logFile(), 'a');
+			if ($handle) {
+                fwrite($handle, $line);
+                fclose($handle);
+            }
 		}
 
+        private function replay() {
+            $db = Symphony::Database();
+            $log_file = self::logFile();
+            if (file_exists($log_file)) {
+                $logs = file_get_contents($log_file);
+                $lines = explode("\n", $logs);
+                $queries = [];
+                foreach ($lines as $line) {
+                    if ($line == '') continue; // skip blank lines
+                    if (substr($line, 0, 2) == '--') continue; // skip comments
+                    $queries[] = $line; // all other lines should be valid queries
+                }
+                if (!$queries) return "No valid queries found to replay in '" . $log_file ."'";
+
+                self::$dont_log_this_run = TRUE; // ensure that the replay of queries aren't logged as new queries
+
+                $count = 0;
+                foreach ($queries as $query){
+                    // if a query fails, fail the whole lot...
+                    if (FALSE === $db->query($query)) return "Error replaying queries. Recommend restore DB from backup."; 
+                    $count++;
+                }
+                if ($count > 0) {
+                    $now = date('Y-m-d-H-i-s');
+                    rename($log_file, $log_file . '.replayed-' . $now); // archive the replayed queries
+                    return ($count . " queries replayed from '" . $log_file . "'");
+                }
+                return "Error: This point should never be reached. File a issue on Github.";
+            }
+            return "No database logs needing to be replayed. Searched in '" . $log_file . "'";
+
+        }
+
+        private function backup() {
+            $mysqldump_bin = Symphony::Configuration()->get('mysqldump_bin', 'db_sync');
+            $gzip_bin = Symphony::Configuration()->get('gzip_bin', 'db_sync');
+            $db_host = Symphony::Configuration()->get('host', 'database');
+            $db_port = Symphony::Configuration()->get('port', 'database');
+            $db_name = Symphony::Configuration()->get('db', 'database');
+            $db_user = Symphony::Configuration()->get('user', 'database');
+            $db_password = Symphony::Configuration()->get('password', 'database');
+
+            $backup_dir = Symphony::Configuration()->get('log_dir', 'db_sync');
+            $backup_filename = $db_name . "-" . date("Y-m-d-H-i-s") . ".sql.gz";
+            $backup_file = DOCROOT . $backup_dir . $backup_filename;
+
+            $return_value = NULL;
+            $result = array();
+            $command = "{$mysqldump_bin} --host={$db_host} --port={$db_port} --user={$db_user} --password={$db_password} {$db_name} | {$gzip_bin} -9 > {$backup_file}";
+            exec($command, $result, $return_value);
+            if ($return_value) {
+                $message = "<strong style='color:red'>ERROR </strong> Unable to backup database: ";
+            }
+            else {
+                $message = "<strong style='color:green'>SUCCESS </strong> Database was backed up to: ";
+            }
+            return $message . "<strong>" . $backup_file . "</strong><br/><br/>" . "Executed command was <em>" . $command . "</em>";
+        }
+
+        private function restore() {
+            $mysql_bin = Symphony::Configuration()->get('mysql_bin', 'db_sync');
+            $gunzip_bin = Symphony::Configuration()->get('gunzip_bin', 'db_sync');
+            $db_host = Symphony::Configuration()->get('host', 'database');
+            $db_port = Symphony::Configuration()->get('port', 'database');
+            $db_name = Symphony::Configuration()->get('db', 'database');
+            $db_user = Symphony::Configuration()->get('user', 'database');
+            $db_password = Symphony::Configuration()->get('password', 'database');
+
+            $backup_file = self::latestBackupFile();
+
+            $return_value = NULL;
+            $result = array();
+            $command = "{$gunzip_bin} < ${backup_file} | {$mysql_bin} --host={$db_host} --port={$db_port} --user={$db_user} --password={$db_password} {$db_name}";
+            exec($command, $result, $return_value);
+
+            if ($return_value) {
+                $message = "<strong style='color:red'>ERROR </strong> Unable to restore database from: ";
+            }
+            else {
+                $message = "<strong style='color:green'>SUCCESS </strong> Database was restored from backup: ";
+            }
+            return $message . "<strong>" . $backup_file . "</strong><br/><br/>" . "Executed command was <em>" . $command . "</em>";
+        }
+
+        private static function latestBackupFile() {
+            $db_name = Symphony::Configuration()->get('db', 'database');
+            $backup_dir = Symphony::Configuration()->get('log_dir', 'db_sync');
+
+            $backups = [];
+            foreach (glob(DOCROOT . $backup_dir . $db_name . "-*") as $backup) {
+                $backups[] = $backup;
+            }
+
+            return array_pop($backups); // already sorted alphabetically by glob()
+        }
+
+        private static function logFile() {
+            $log_file = Symphony::Configuration()->get('log_file', 'db_sync');
+            $log_dir = Symphony::Configuration()->get('log_dir', 'db_sync');
+            return DOCROOT . $log_dir . $log_file;
+        }
 	}
